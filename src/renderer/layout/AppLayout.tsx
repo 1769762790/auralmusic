b212df495703f12251098ede8130e4203bf18a1f
shizhaohai <alexhai@ripple.one>
2026-04-19 20:29:53 +0800

    style: fix mac style padding

--- src/renderer/layout/AppLayout.tsx
+++ src/renderer/layout/AppLayout.tsx
@@ -21,7 +21,7 @@ const AppLayout = () => {
   return (
     <main
       className={`w-full px-12 pb-[100px] xl:px-25 2xl:px-50 ${
-        isWindows ? 'pt-20' : 'pt-16.25'
+        isWindows ? 'pt-20' : 'pt-25'
       }`}
     >
       <Header className='window-drag fixed top-0 right-0 left-0 z-50 pt-5 pb-1.25' />
